6e891e3701a18c55dc9d159ee377a1e7ae088edf
이광석 <lee.kwangsug@gmail.com>
2026-01-05 03:44:01 +0000

    오른쪽에 스크롤 바가 안보이는데. 그래서 안되는 거 아니야?

--- src/components/game/store-modal.tsx
+++ src/components/game/store-modal.tsx
@@ -26,9 +26,9 @@ export default function StoreModal() {
           <DialogTitle className="font-headline text-2xl text-center">🛒 {t('store_title')}</DialogTitle>
           <DialogDescription className="text-center">{t('store_subtitle')}</DialogDescription>
         </DialogHeader>
-        <div className="flex-grow min-h-0">
-          <ScrollArea className="h-full pr-4">
-            <div className="space-y-3">
+        <div className="relative flex-1 -mx-6 overflow-hidden">
+          <ScrollArea className="h-full px-6">
+            <div className="space-y-3 pb-6">
               {state?.items.map((item, index) => (
                 <ItemCard key={item.id} item={item} index={index} />
               ))}
