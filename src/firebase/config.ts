93994ad051df42a317f930c953d6c5ebf3356571
이광석 <lee.kwangsug@gmail.com>
2026-01-05 14:02:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3446891698-7dbe0",
+  "appId": "1:489775542126:web:08fab21997dab2d6559b7f",
+  "apiKey": "AIzaSyCTcMXWKmbDSnptTysNyQ6zpOIkY9p0u_w",
+  "authDomain": "studio-3446891698-7dbe0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489775542126"
+};
