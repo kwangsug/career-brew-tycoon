b39e070d354ba29cc487f70750c5a24a5f113b72
이광석 <lee.kwangsug@gmail.com>
2026-01-05 06:05:41 +0000

    상점 캔버스 안의 상품 목록 코드를 다시 짜 줘요. 스크롤이 안돼. 처음부터 다시 짜면 좋겠어

--- src/components/game/store-modal.tsx
+++ src/components/game/store-modal.tsx
@@ -26,13 +26,15 @@ export default function StoreModal() {
           <DialogTitle className="font-headline text-2xl text-center">🛒 {t('store_title')}</DialogTitle>
           <DialogDescription className="text-center">{t('store_subtitle')}</DialogDescription>
         </DialogHeader>
-        <ScrollArea className="flex-grow -mx-6">
-          <div className="px-6 space-y-3 pb-6">
-            {state?.items.map((item, index) => (
-              <ItemCard key={item.id} item={item} index={index} />
-            ))}
-          </div>
-        </ScrollArea>
+        <div className="flex-grow overflow-hidden -mx-6">
+          <ScrollArea className="h-full px-6">
+            <div className="space-y-3 pb-6">
+              {state.items.map((item, index) => (
+                <ItemCard key={item.id} item={item} index={index} />
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
       </DialogContent>
     </Dialog>
   );
